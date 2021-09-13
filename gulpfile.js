2956671b68eccafb67fd05234a3963f313a1c26f
patelsaheb1219 <kaumik1219@gmail.com>
2021-09-13 10:31:37 -0400

    Updated the project structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 const buildStyles = () => {
-  return src('*.scss')
+  return src('coconut/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 const watchTask = () => {
-  watch(['*.scss'], buildStyles);
+  watch(['coconut/**/*.scss'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
